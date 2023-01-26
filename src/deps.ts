dbbb1c6832c8a7b8d3ce2bc05479e2e98e248bba
Nick Hrones <nhrones@users.noreply.github.com>
2023-01-25 20:10:19 -0500

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
 
-export { serve } from "https://deno.land/std@0.173.0/http/server.ts";
-export { join } from "https://deno.land/std@0.173.0/path/mod.ts";
+export { serve } from "https://deno.land/std@0.174.0/http/server.ts";
+export { join } from "https://deno.land/std@0.174.0/path/mod.ts";
 export type { WalkEntry } from "https://deno.land/std@0.167.0/fs/mod.ts"
-export { walk } from "https://deno.land/std@0.173.0/fs/mod.ts?s=walk"
+export { walk } from "https://deno.land/std@0.174.0/fs/mod.ts?s=walk"
